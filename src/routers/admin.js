119a3b82905752c67449af6c6e1cd4039e31c469
Mark <50715288+whymeoff@users.noreply.github.com>
2020-03-01 23:34:08 +0200

    Routes to controllers

--- src/routers/admin.js
+++ src/routers/admin.js
@@ -1,6 +1,5 @@
 const express = require('express')
-const User = require('../models/user')
-const Product = require('../models/product')
+const controllers = require('../controllers/admin')
 
 const router = express.Router()
 
@@ -9,99 +8,17 @@ router.get('/', (req, res) => {
 })
 
 // Staff routes
-router.post('/createStaff', async (req, res) => {
-    const user = new User({...req.body})
-    
-    try {
-        await user.save()
-        return res.status(201).send(user)
-    } catch (e) {
-        return res.status(400).send()
-    }
-})
+router.post('/createStaff', controllers.createStaff)
 
-router.patch('/editStaff/:id', async (req, res) => {
-    const allowedUpdates = ['fullName', 'age', 'role', 'email', 'password']
-    const updates = Object.keys(req.body)
+router.patch('/editStaff/:id', controllers.editStaff)
 
-    const updatesBool = updates.every((update) => {
-        return allowedUpdates.includes(update)
-    })
-    
-    if (!updatesBool) return res.status(400).send({error: 'Invalid updates'})
-
-    try {
-        const user = await User.findById(req.params.id)
-        if (!user) return res.status(404).send()
-
-        updates.forEach((key) => {
-            user[key] = req.body[key]
-        })
-
-        await user.save()
-        res.send(user)
-    } catch (e) {
-        return res.status(400).send()
-    }
-})
-
-router.delete('/deleteStaff/:id', async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        if (!user) return res.status(404).send()
-
-        return res.send(user)
-    } catch (e) {
-        return res.status(400).send()
-    }
-})
+router.delete('/deleteStaff/:id', controllers.deleteStaff)
 
 // Product routes
-router.post('/createProduct', async (req, res) => {
-    const product = new Product({...req.body})
+router.post('/createProduct', controllers.createProduct)
 
-    try {
-        await product.save()
-        return res.status(201).send(product)
-    } catch (e) {
-        return res.status(400).send()
-    }
-})
+router.patch('/editProduct/:id', controllers.editProduct)
 
-router.patch('/editProduct/:id', async (req, res) => {
-    const allowedUpdates = ['name', 'description', 'price']
-    const updates = Object.keys(req.body)
-
-    const updatesBool = updates.every((update) => {
-        return allowedUpdates.includes(update)
-    })
-    
-    if (!updatesBool) return res.status(400).send({error: 'Invalid updates'})
-
-    try {
-        const product = await Product.findById(req.params.id)
-        if (!product) return res.status(404).send()
-
-        updates.forEach((key) => {
-            product[key] = req.body[key]
-        })
-
-        await product.save()
-        res.send(product)
-    } catch (e) {
-        return res.status(400).send()
-    }
-})
-
-router.delete('/deleteProduct/:id', async (req, res) => {
-    try {
-        const product = await Product.findByIdAndDelete(req.params.id)
-        if (!product) return res.status(404).send()
-
-        return res.send(product)
-    } catch (e) {
-        return res.status(400).send()
-    }    
-})
+router.delete('/deleteProduct/:id', controllers.deleteProduct)
 
 module.exports = router
--- src/routers/seller.js
+++ src/routers/seller.js
@@ -1,32 +1,10 @@
 const express = require('express')
-const Order = require('../models/order')
+const controllers = require('../controllers/seller')
 
 const router = express.Router()
 
-router.get('/orders', async (req, res) => {
-    try {
-        const orders = await Order.find({ state: 0 })
-        if (!orders) return res.status(404).send()
+router.get('/orders', controllers.getOrders)
 
-        return res.send(orders)
-    } catch (e) {
-        return res.status(500).send()
-    }
-})
-
-router.patch('/orderExecuted/:id', async (req, res) => {
-    try {
-        const order = await Order.findById(req.params.id)
-        if (!order) throw new Error
-        if (order.state !== 0) throw new Error
-        order.state = 1 // State 1 = order executed
-    
-        await order.save()
-    
-        return res.send()
-    } catch (e) {
-        return res.status(400).send()
-    }
-})
+router.patch('/orderExecuted/:id', controllers.orderExecuted)
 
 module.exports = router
