4c2d53b5bfb6136c8906f15daf959b30edc55783
Mark <50715288+whymeoff@users.noreply.github.com>
2020-03-02 20:53:37 +0200

    Discount function

---
+++ src/utils/discount.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/shop-api', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+
+const Product = require('../models/product')
+
+const discount = async () => {
+    try {
+        const products = await Product.find({})
+        const date = new Date().getDate()
+        const ms = 2592000000
+
+        for (let i = 0; i < products.length; i++) {
+            if (product.creationDate + ms > date  ) {
+                await Product.findByIdAndUpdate(product._id, { discount: true })
+            }
+        }
+
+    } catch (e) {
+        console.log(e)
+    }
+
+    await mongoose.disconnect()
+}
+
+discount()
